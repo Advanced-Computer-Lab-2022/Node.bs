d6c2d49945d8c9133af3902b6c210e5f2c0965e5
Mohamed Emad <mez_emz@yahoo.com>
2022-10-21 03:21:06 +0200

    fix and add more boilerplate code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,20 @@
+//Imports
 require('dotenv').config();
-console.log(process.env.PORT);
 const express = require('express');
 
 //create express app
 const app = express();
+
+//JSON body parser middleware
+app.use(express.json());
+
 //port
 const port = process.env.PORT || '8000';
+
 //listen for requests
 app.listen(port, () => {
   console.log('listening on port 4000');
 });
-let omar = 'omar';
+
+//Mongo URI
+const MongoURI = process.env.MONGO_URI;
