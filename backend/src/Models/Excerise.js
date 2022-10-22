5983c98aa5395c85b34a1b33ddedd009dd44d0ba
Omar Melouk <omarmelouk@Omars-MBP.home>
2022-10-23 00:36:54 +0200

    added changes, timestamps

--- backend/src/Models/Excerise.js
+++ backend/src/Models/Excerise.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
-const ExerciseSchema = new schema({
-  type: {
-    type: String,
-  },
-
-  options: [{
-      type: String,
-    }
-  ],
-
-  questions: {
-    type: String,
-  },
-  answers: [
-    {
+const ExerciseSchema = new schema(
+  {
+    type: {
       type: String,
     },
-  ],
-});
+
+    options: [
+      {
+        type: String, //must be of size 4
+      },
+    ],
+
+    questions: {
+      type: String,
+    },
+    answers: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 module.exports = mongoose.model('Exercise', ExerciseSchema);
