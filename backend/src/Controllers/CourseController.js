30b1477a225cb1047d19ec955068d4e8f8d0139c
Omar Melouk <omarmelouk@Omars-MacBook-Pro.local>
2022-10-24 01:58:54 +0200

    added course controllers

---
+++ backend/src/Controllers/CourseController.js
@@ -0,0 +1,49 @@
+const Course = require('../Models/Course');
+
+//get all Courses
+const getAllCourses = async (req, res) => {
+  const result = await Course.find();
+
+  if (result) {
+    return res.status(200).json(result);
+  }
+
+  res.status(404).json({ message: 'no result found' });
+};
+
+//filter courses
+const filterCourses = async (req, res) => {
+  const result = await Course.find(req.body.query);
+
+  if (result) {
+    return res.status(200).json(result);
+  }
+
+  res.status(404).json({ message: 'no result found' });
+};
+
+//seach courses
+const searchCourses = async (req, res) => {
+  const result = await Course.find(req.body.query).populate({
+    path: req.body.extQuery.toBePopulated,
+    match: req.body.extQuery.query,
+  });
+
+  if (result) {
+    return res.status(200).json(result);
+  }
+
+  res.status(404).json({ message: 'no result found' });
+};
+
+//create Course
+const createCourse = async (req, res) => {
+  try {
+    const result = await Course.create(req.body.course);
+    res.status(203).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
