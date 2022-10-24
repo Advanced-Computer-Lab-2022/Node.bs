a509bed7eaa76be454f3deee4d040626b4ef1e00
Mohamed Emad <43939754+iMezemz@users.noreply.github.com>
2022-10-24 02:20:06 +0200

    Mez branch (#9)
    * added optimized course controller methods

--- backend/src/Models/IndividualTrainee.js
+++ backend/src/Models/IndividualTrainee.js
@@ -1,49 +1,52 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
-const IndividualTraineeSchema = new schema({
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  username: {
-    type: String,
-  },
-
-  password: {
-    type: String,
-  },
-  registeredCourses: [
-    {
-      course: { type: schema.Types.ObjectId, ref: 'Course' },
-      submissions: [{ type: schema.Types.ObjectId, ref: 'Submission' }],
-      progress: { type: Number },
-    },
-  ],
-  wallet: {
-    type: Number,
-  },
-  verified: {
-    type: Boolean,
-  },
-
-  creditCard: {
-    cardNumber: { type: String },
-    expiryDate: { type: Date },
-  },
-  pathToCertificate: {
-    type: String,
-  },
-  notebook: [
-    {
+const IndividualTraineeSchema = new schema(
+  {
+    firstName: {
       type: String,
     },
-  ],
-}, {timestamps: true});
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    username: {
+      type: String,
+    },
+
+    password: {
+      type: String,
+    },
+    registeredCourses: [
+      {
+        course: { type: schema.Types.ObjectId, ref: 'Course' },
+        submissions: [{ type: schema.Types.ObjectId, ref: 'Submission' }],
+        progress: { type: Number },
+      },
+    ],
+    wallet: {
+      type: Number,
+    },
+    verified: {
+      type: Boolean,
+    },
+
+    creditCard: {
+      cardNumber: { type: String },
+      expiryDate: { type: Date },
+    },
+    pathToCertificate: {
+      type: String,
+    },
+    notebook: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('IndividualTrainee', IndividualTraineeSchema);
