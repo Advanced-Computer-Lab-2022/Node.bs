e2eb0e75287ae3398be0dd85109fbc1c7e65c1e8
Omar Melouk <omarmelouk@Omars-MacBook-Pro.local>
2022-10-30 05:20:03 +0200

    added course and admin services

---
+++ frontend/src/services/AdminService.js
@@ -0,0 +1,13 @@
+import BaseAxios from "../Requester/BaseAxios";
+
+export const createAdmin = (admin) => {
+  return BaseAxios.post("/admin", admin);
+};
+
+export const createCorporateTrainee = (trainee) => {
+  return BaseAxios.post("/admin/corporate", trainee);
+};
+
+export const createInstructor = (instructor) => {
+  return BaseAxios.get("/admin/instructor", instructor);
+};
